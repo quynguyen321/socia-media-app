8a837a4b1798e4f610d7755cc6b51bc95f7df0d8
Quy Nguyen <quy.ybu@gmail.com>
2022-06-04 10:54:41 -0500

    trend card donee

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Posts from '../Posts/Posts'
 import PostShare from '../PostShare/PostShare'
 import './PostSide.css'
 function PostSide() {
   return (
     <div className="PostSide">
         <PostShare/>
+        <Posts/>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import"./Home.css"
 import ProfileSide from '../../components/Profile/ProfileSide'
 import PostSide from '../../components/PostSide/PostSide'
+import RightSide from '../../components/RightSide/RightSide'
 
 const Home = () => {
   return (
     <div className="Home">
         <ProfileSide/>
         <PostSide/>
-        <div className="rightSide">Rightside</div>
+        <RightSide/>
     </div> 
   )
 }
